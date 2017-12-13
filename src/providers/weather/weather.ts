42a05c6da931e9de4e4158714e627cd6c0d5e292
Juan Aguirre <Jaguirr10@gmail.com>
2017-12-13 03:14:01 -0500

    initial commit

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class WeatherProvider {
+  apiKey = 'a636a3cb0ec5a567';
+  url;
+
+  constructor(public http: Http) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'https://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+  }
+
+  getWeather(city, state){
+    return this.http.get(this.url+'/'+state+'/'+city+'.json')
+      .map(res => res.json());
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TabsPage } from '../pages/tabs/tabs';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = TabsPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+  }
+} */
